e4d51fda6610dff55d6bb6c916782aa15894946c
irajkovic1978 <irajkovic@tvz.hr>
2025-03-25 22:38:44 +0100

    Add files via upload

---
+++ 1_promjenjive.py
@@ -0,0 +1,19 @@
+# promjenjive ili varijable
+# ime = "Ivan"
+# godine = "46"
+# visina = "181"
+# print (ime + " je visok " + visina + "cm" +  " i ima " + godine + " godina")
+# ime = input("Unesi ime")
+# godine = int(input ("Unesi godine"))
+# visina = input ("Unesi visinu")
+# print (ime)
+# print (godine)
+# print (visina)
+
+num1= int (input("Unesi prvi broj:"))
+num2 = int (input ("Unesi drugi broj:"))
+rezultat = num1 + num2
+print ("Rezultat zbrajanje je", rezultat)
+
+
+

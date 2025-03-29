df77955399229fb8f200a8452c9fd503e81648c8
irajkovic1978 <irajkovic@tvz.hr>
2025-03-29 23:24:01 +0100

    Add files via upload

---
+++ 5_uslovne_naredbe.py
@@ -0,0 +1,43 @@
+#### KONTROLE TOKA #####
+
+# Petlje
+    # while i for petlja
+# naredbe za skakanje
+    # break i continue
+# Uslovne naredbe
+    # if    elif    else
+
+python = True
+if python == True:
+    print ("Python je savršen jezik za započeti programiranje")
+
+x = 140
+y = 66
+if x < y:
+    print ("Izraz je tačan")
+else:
+    print("Izraz nije tačan")
+
+
+umoran = False
+if umoran == True:
+    print ("Idi spavaj i odmori se - sutra je novi dan")
+else:
+    print ("Odlično, idi dalje na aktivnosti")
+
+
+x = 29
+if(x%2 == 0):
+    print ("Ovo je paran broj")
+else:
+    print ("Ovo je neparan broj")
+
+
+x = 209
+y = 209
+if (x<y):
+    print ("X je manji od Y")
+elif x == y:
+    print ("X i Y su jednaki")
+else:
+    print ("X je veće od Y")

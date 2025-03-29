2a00f3db1fe836fd1e4d3d3711b80fc1db964394
irajkovic1978 <irajkovic@tvz.hr>
2025-03-29 01:36:24 +0100

    Add files via upload

---
+++ 2_tipovi_podataka.py
@@ -0,0 +1,61 @@
+# TIPOVI PODATAKA
+
+# int #
+a = 3
+b = 200
+c = -200
+print (a,b,c)
+print (type (a))
+
+# float #
+pi = 3.14
+x =4.7
+y = -299.00
+print (pi, x,y)
+print (type(pi))
+
+# complex #
+x =2+3j
+y = 1.5 - 4.8j
+print (x)
+print (y)
+print (type(x))
+
+# stringovi #
+a = "Python je totalno ludilo, naučite ga"
+print (a)
+print (type (a))
+
+# booleans #
+print (True)
+print (False)
+print (type(True))
+
+#liste #
+liste = [100,  "Python", 3.14, True, False, ["1", "2", "3"]]
+#          0        1       2   3       4           5
+#                                           [0      1    2 ]
+print(liste)
+print (type(liste))
+
+# dictionaries, riječnici #
+kursevi = {"kurs":"python za početnike", "predavač": "Sloba"}
+print (kursevi)
+print (type(kursevi))
+
+# tuol (torke) #
+kurs = ("python", 3, "mjeseca")
+#           0      1    2
+print (kurs[0] + " traje " + str(kurs[1]) + " " + kurs[2])
+print(type(kurs))
+
+
+# sets (skupovi) #
+a = {"a", "e", "i", "o", "u"}
+print(type(a))
+print(a)
+
+
+
+
+
